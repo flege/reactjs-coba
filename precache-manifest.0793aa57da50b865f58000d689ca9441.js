b2b6a15d0073fa017eb099103364935084c3407a
FLEGE <abd.alfaruq@GMAIL.COM>
2019-03-11 12:58:07 +0700

    Updates

---
+++ precache-manifest.0793aa57da50b865f58000d689ca9441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reactjs-coba/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "695f9c94849a17657c72",
+    "url": "/reactjs-coba/static/js/runtime~main.37adafe2.js"
+  },
+  {
+    "revision": "8c8fe36a919ff8d32a27",
+    "url": "/reactjs-coba/static/js/main.0da4c275.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/reactjs-coba/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "8c8fe36a919ff8d32a27",
+    "url": "/reactjs-coba/static/css/main.28429657.chunk.css"
+  },
+  {
+    "revision": "d38fa24a50da20b29e446ead14931df9",
+    "url": "/reactjs-coba/index.html"
+  }
+];
