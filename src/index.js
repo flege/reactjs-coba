4bd4171bdb21cb97ad684b307b5463fdaf479cc6
flege <abd.alfaruq@gmail.com>
2019-03-21 07:36:22 +0700

    rename to apple

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 const items = [
     {
-        name : 'Apel',
+        name : 'Apple',
         color : 'Merah',
         img   : 'apel.jpg',
     },
