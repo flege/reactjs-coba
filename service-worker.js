a9ce5d09970b16a88248554d3d0625e2d7951f8f
flege <abd.alfaruq@gmail.com>
2019-04-15 13:56:37 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactjs-coba/precache-manifest.173a5028dfdeb0098635397fcf34d863.js"
+  "/reactjs-coba/precache-manifest.9e0cb663194132f84261ad80485517c8.js"
 );
 
 workbox.clientsClaim();
