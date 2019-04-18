bb9ceee685c3f487640cb987d0955477311a227f
flege <abd.alfaruq@gmail.com>
2019-04-18 08:57:54 +0700

    calculator complete

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       img: 'apel.jpg',
       angka1: 0,
       angka2: 0,
+      operation: 'plus',
     };
   }
 
@@ -29,9 +30,15 @@ class App extends Component {
       return (
         <div>
           <span className={"selected "+this.state.color}>{this.state.name}  </span><br/><br/>
-          <input type="number" name="angka1" placeholder="angka 1"  value={this.state.angka1} onChange={this.changeHandler.bind(this)}/>+
+          <input type="number" name="angka1" placeholder="angka 1"  value={this.state.angka1} onChange={this.changeHandler.bind(this)}/>
+          <select name="operation" onChange={this.changeHandler.bind(this)}>
+            <option value="plus">+</option>
+            <option value="minus">-</option>
+            <option value="pow">x</option>
+            <option value="div">:</option>
+          </select>
           <input type="number" name="angka2" placeholder="angka 2"  value={this.state.angka2} onChange={this.changeHandler.bind(this)}/>=
-          <span> {parseInt(this.state.angka1) + parseInt(this.state.angka2)}</span>
+          <span> {this.hitung()}</span>
         </div>
       );
     }else{
@@ -53,6 +60,14 @@ class App extends Component {
     this.setState({
       [event.target.name]: event.target.value 
     });
+  }
+  hitung(){
+    switch(this.state.operation){
+      case 'plus': return parseInt(this.state.angka1) + parseInt(this.state.angka2);
+      case 'minus': return parseInt(this.state.angka1) - parseInt(this.state.angka2)
+      case 'pow': return parseInt(this.state.angka1) * parseInt(this.state.angka2)
+      case 'div': return parseInt(this.state.angka1) / parseInt(this.state.angka2)
+    }
   }
   // menusaya() {
   //   var gambar;
